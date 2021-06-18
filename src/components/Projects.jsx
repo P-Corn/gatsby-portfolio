1eadbaa97fac6c5245f65a9d2f403ea20b4c1a41
Peyton Cornelison <peytoncornelison@gmail.com>
2021-06-18 16:52:58 -0500

    project cards created

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,23 +3,24 @@ import SectionHeader from './utils/SectionHeader';
 import SkillSpan from "./utils/SkillSpan";
 
 export default function Projects() {
-  const tempSkills = ['HTML', 'CSS', 'React', 'JavaScript', 'Material-UI'];
+  const tempSkills = ['HTML5', 'CSS3', 'ReactJS', 'JavaScript', 'Material-UI'];
 
-  const ProjectCard = ({skills}) => (
-    <div className="card">
+  const ProjectCard = ({title, desc, skills}) => (
+    <div className="card mb-10">
       <div>
-        <h2 className="text-3xl text-bodyText font-normal">Vottron</h2>
+        <h2 className="text-3xl text-bodyText font-normal mb-8">{title}</h2>
       </div>
-      <div>
-        <p className="text-bodyText">Here's a description about the project such as it's purpose, what I learned, and the tools I used to build it.</p>
+      <div className="mb-8">
+        <p className="text-bodyText">{desc}</p>
       </div>
-      <div>
+      <div className="mb-8">
         {skills.map(skill => (
           <SkillSpan skill={skill}/>
         ))}
       </div>
       <div>
-        <button className="btn bg-primary text-white">Live demo</button>
+        <button className="btn bg-primary text-white mb-4">Live demo</button>
+        <br/>
         <button className="btn text-primary">View source</button>
       </div>
     </div>
@@ -29,7 +30,9 @@ export default function Projects() {
     <div className="bg-section section">
       <SectionHeader title="Projects"/>
       <div>
-        <ProjectCard skills={tempSkills}/>
+        <ProjectCard title="Vottron" desc="Here's a description about the project such as it's purpose, what I learned, and the tools I used to build it." skills={tempSkills}/>
+        <ProjectCard title="Spotifind" desc="Here's a description about the project such as it's purpose, what I learned, and the tools I used to build it." skills={tempSkills}/>
+        <ProjectCard title="Portfolio" desc="Here's a description about the project such as it's purpose, what I learned, and the tools I used to build it." skills={tempSkills}/>
       </div>
     </div>
   )
