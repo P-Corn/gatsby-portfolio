5f5849962ced6902bc3a0e433c6746d212151035
Peyton Cornelison <peytoncornelison@gmail.com>
2021-07-08 16:57:55 -0500

    used emailjs to make the contact form functional

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,29 +1,109 @@
-import React from "react";
+import React, {useState} from "react";
 import SectionHeader from './utils/SectionHeader';
+import emailjs from 'emailjs-com';
+
+const Input = ({inputName, type, placeholder, value, onChange, textArea = false}) => {
+  if(!textArea)
+    return (
+    <input 
+      type={type} 
+      name={inputName}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder} 
+      aria-label={placeholder}
+      className="rounded pl-2 py-5 border h-8 w-full border-transparent shadow-inner focus:outline-none focus:ring-2 focus:ring-green focus:border-transparent"/>
+    )
+  
+  return (
+  <textarea 
+    rows="6" 
+    type={type}
+    name={inputName}
+    value={value}
+    onChange={onChange}
+    placeholder={placeholder} 
+    aria-label={placeholder} 
+    className="rounded pl-2 py-2 border w-full border-transparent shadow-inner focus:outline-none focus:ring-2 focus:ring-green focus:border-transparent"/>
+  )
+}
 
 export default function Contact() {
 
-  const Input = ({name, type, placeholder, textArea = false}) => {
-    if(!textArea)
-      return <input type={type} name={name} placeholder={placeholder} aria-label={placeholder} className="rounded pl-2 py-5 border h-8 w-full border-transparent shadow-inner focus:outline-none focus:ring-2 focus:ring-green focus:border-transparent"/>
-    
-    return <textarea rows="5" type={type} name={name} placeholder={placeholder} aria-label={placeholder} className="rounded pl-2 py-2 border w-full border-transparent shadow-inner focus:outline-none focus:ring-2 focus:ring-green focus:border-transparent"/>
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleChange = (event) => {
+    if (event.target.name === "name") {
+      setName(event.target.value);
+    } else if (event.target.name === "email") {
+      setEmail(event.target.value);
+    } else {
+      setMessage(event.target.value);
+    }
+  }
+
+  const formMessage = document.querySelector('.form-message');
+  const handleSuccess = () => {
+    setName('');
+    setEmail('');
+    setMessage('');
+
+    formMessage.classList.remove('opacity-0')
+    setTimeout(() => {
+      formMessage.classList.add('opacity-0')
+    }, 3000)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('portfolio_site', 'portfolio_site', e.target, 'user_WDObBAlbPYJe8ZiZ6VQ9w')
+      .then(() => {
+        handleSuccess();
+      }, (error) => {
+        throw new Error(error.text);
+      });
   }
 
   return (
     <div className="pb-28">
       <div className="section">
         <SectionHeader title="Contact"/>
-        <form className="card">
+        <form onSubmit={handleSubmit} className="card">
           <div className="mb-8">
-            <h2 className="text-bodyText sm:text-lg text-center">Want to work with me? <br className="sm:hidden"/> <span className="border-b-2 border-green">Send me a message.</span></h2>
+            <h2 className="text-bodyText sm:text-lg text-center">
+              Want to work with me? <br className="sm:hidden"/> 
+              <span className="border-b-2 border-green">Send me a message.</span>
+            </h2>
           </div>
-          <Input type="text" name="name" placeholder="Your name"/>
+          <Input
+            type="text" 
+            inputName="name"
+            value={name}
+            onChange={handleChange}
+            placeholder="Your name"
+          />
           <div className="mb-8"/>
-          <Input type="email" name="email" placeholder="Your email"/>
+          <Input 
+            type="email" 
+            inputName="email" 
+            value={email} 
+            onChange={handleChange}
+            placeholder="Your email"
+          />
           <div className="mb-8"/>
-          <Input type="textarea" name="message" placeholder="Your message" textArea="true"/>
-          <div className="mb-8"/>
+          <Input 
+            type="textarea"
+            value={message}
+            onChange={handleChange}
+            inputName="message" 
+            placeholder="Your message" 
+            textArea="true"
+          />
+          <div className="form-message transition-all duration-300 ease-in-out bg-green w-full text-center text-bodyText opacity-0">Message sent!</div>
+          <div className="mb-2"/>
           <div className="w-full flex justify-end">
             <button className="btn bg-primary text-white shadow-md hover:shadow-lg">SUBMIT</button>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -30,7 +30,7 @@ module.exports = {
       lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
       xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
       '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
-     '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
       inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
       none: 'none',
       card: '0 50px 100px rgb(50 50 93 / 5%), 0 15px 35px rgb(50 50 93 / 10%), 0 5px 15px rgb(0 0 0 / 10%)'
@@ -51,6 +51,11 @@ module.exports = {
 
       '2xl': '1536px',
       // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      transitionProperty: {
+       'height': 'height',
+      }
     }
   },
   variants: {
