f9d725a1f8dae260abdbc402668a834fa59bf7ba
Peyton Cornelison <peyton.corn8@gmail.com>
2021-12-27 18:10:54 -0600

    changed the hr under section header

--- src/components/utils/SectionHeader.jsx
+++ src/components/utils/SectionHeader.jsx
@@ -4,7 +4,7 @@ export default function SectionHeader({title}) {
     return (
       <div className="text-center">
         <h2 className="section-title">{title}</h2>
-        <hr className="w-16 border-2 border-primary mx-auto mt-3 mb-9"></hr>
+        <hr className="w-16 border-0 bg-primary py-0.5 mx-auto mt-3 mb-9"></hr>
       </div>
     )
   }
